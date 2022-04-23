53e0a5da80fe6fa7ada2de2929c051a7f017f4e8
burakceylanz <91959780+burakceylanz@users.noreply.github.com>
2022-04-23 04:33:54 +0300

    Update ProductItem.js

--- src/components/ui/ProductItem.js
+++ src/components/ui/ProductItem.js
@@ -7,7 +7,7 @@ function ProductItem({product:{id,title,image,alt,price}}) {
       <button className='absolute bg-white top-3 right-3 w-8 h-8 flex items-center justify-center border border-gray-200 rounded-lg text-primary-brand-color shadow-lg shadow-gray-500/10'>
         <AiOutlinePlus/>
       </button>
-      <img src={image} alt={title} className=''/>
+      <img src={image} alt={title} className='w-40'/>
       <h3 className='text-brand-color font-semibold items-center'>₺{price}</h3>
       <span className='font-sm font-[500] text-gray-900'>{title}</span>
       <span className='text-gray-400'>{alt}</span>
