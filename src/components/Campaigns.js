fa0cb52bb64a2f9098842880f0b10567c420dbc5
burakceylanz <91959780+burakceylanz@users.noreply.github.com>
2022-04-23 04:32:43 +0300

    Update Campaigns.js

--- src/components/Campaigns.js
+++ src/components/Campaigns.js
@@ -12,7 +12,7 @@ function Campaigns() {
   return (
       <div className='container mx-auto pt-12'>
          <Title>Kampanyalar</Title>
-        <div className='grid grid-cols-3'>
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-3'>
           {banner.length && banner.map((banners,index)=>(
             <div key={index} className='p-2'><img src={banners.image} className='rounded-lg'/></div>
           ))}
